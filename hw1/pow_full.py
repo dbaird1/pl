bfc3ae44d777e691b5e20e3b2d119cec31400f41
david <david@localhost.localdomain>
2016-02-03 18:55:15 -0500

    4 done

--- hw1/pow_full.py
+++ hw1/pow_full.py
@@ -2,14 +2,23 @@
 
 import sys
 
-def pow(n, p):
+def powF(n, p):
   if p == 0:
     return 1
   else:
     return n * pow(n, p-1) 
 
+def powI(n,p):
+  if p == 0:
+    return 1;
+  s = 1
+  while p > 0:
+    p-=1
+    s= s*n
+  return s
 if len(sys.argv) != 3:
   print("%s usage: [NUMBER] [power]" % sys.argv[0])
   exit()
 
-print(pow(int(sys.argv[1]), int(sys.argv[2])))
+print(powF(int(sys.argv[1]), int(sys.argv[2])))
+print(powI(int(sys.argv[1]), int(sys.argv[2])))
--- hw1/pow_full.rb
+++ hw1/pow_full.rb
@@ -1,10 +1,23 @@
 #! /usr/bin/env ruby
-def pow(n, p)
+def powF(n, p)
   if(p == 0)
     return 1
   end
 
-  return n * pow(n, p-1)
+  return n * powF(n, p-1)
+end
+
+def powI(n, p)
+  if(p == 0)
+    return 1
+  end
+
+  s = 1
+  while p > 0 do
+    p = p-1
+    s=s*n
+  end
+  return s
 end
 
 if ARGV.length != 2
@@ -12,4 +25,5 @@ if ARGV.length != 2
   exit
 end
 
-puts pow(ARGV[0].to_i, ARGV[1].to_i)
+puts powF(ARGV[0].to_i, ARGV[1].to_i)
+puts powI(ARGV[0].to_i, ARGV[1].to_i)
